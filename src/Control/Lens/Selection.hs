35dee28b8c2f7dba87b1c87e2e779c148fd65a18
Chris Penner <christopher.penner@gmail.com>
2017-08-11 08:05:32 -0600

    Fix 'unselected' lens

--- src/Control/Lens/Selection.hs
+++ src/Control/Lens/Selection.hs
@@ -2,7 +2,7 @@
 {-# language RankNTypes #-}
 module Control.Lens.Selection
   ( selected
-  -- , unselected
+  , unselected
   , inverting
   , unwrapping
   ) where
@@ -19,29 +19,23 @@ type Iso' s a = Iso s s a a
 -- | Traversal over selected elements
 --
 -- @'selected' = 'traverse'@
-selected :: (Selectable s f b, Traversable s, Selectable t f b, Traversable f) => Traversal (s a) (t c) a c
+selected :: (Selectable s f b, Traversable s, Traversable f) => Traversal (s a) (s c) a c
 selected f = fmap wrapSelection . traverse (bitraverse pure f) . unwrapSelection
 
 -- -- | Traversal over unselected elements
 -- --
 -- -- @'unselected' = 'inverting' . 'selected'@
--- unselected :: (Selectable s f b, Selectable t f c) => Traversal (s a) (s a) b c
--- unselected = inverting . selected
---   where
---     inv :: Iso' (s a) (t b)
---     inv = inverting
---     sel :: Traversal (t b) (s a) b a
---     sel = selected
-
+unselected :: (Selectable s f b, Selectable t f c, Traversable f) => Traversal (s a) (t a) b c
+unselected f = fmap wrapSelection . traverse (bitraverse f pure) . unwrapSelection
 
 -- | Iso which inverts the current selection
 
 -- @'inverting' = iso 'invertSelection' 'invertSelection'@
 inverting :: forall s t a b f. (Selectable s f b, Selectable t f a) => Iso' (s a) (t b)
-inverting = dimap (invertSelection :: s a -> t b) (fmap invertSelection)
+inverting = dimap invertSelection (fmap invertSelection)
 
 -- | Iso which exposes the underlying functor representation
 --
 -- @'unwrapping' = iso 'unwrapSelection' 'wrapSelection'@
-unwrapping :: (Functor f, Selectable s f b, Selectable t g d) => Iso (s a) (t c) (f (Either b a)) (g (Either d c))
+unwrapping :: (Functor f, Selectable s f b, Selectable t f d) => Iso (s a) (t c) (f (Either b a)) (f (Either d c))
 unwrapping = dimap unwrapSelection (fmap wrapSelection)

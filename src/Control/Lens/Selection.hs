41c6ee335d0702a853a2d5bd5be8b29ab966bd05
Chris Penner <christopher.penner@gmail.com>
2017-08-12 11:41:38 -0600

    Simplify Lens types

--- src/Control/Lens/Selection.hs
+++ src/Control/Lens/Selection.hs
@@ -18,24 +18,24 @@ type Iso' s a = Iso s s a a
 -- | Traversal over selected elements
 --
 -- @'selected' = 'traverse'@
-selected :: (Traversable f) => Traversal (Selection f b a) (Selection f b c) a c
+selected :: (Traversable f) => Traversal' (Selection f b a) a
 selected = traverse
 
 -- | Traversal over unselected elements
 --
 -- @'unselected' = 'inverting' . 'selected'@
-unselected :: (Traversable f) => Traversal (Selection f b a) (Selection f c a) b c
+unselected :: (Traversable f) => Traversal' (Selection f b a) b
 unselected = inverting . selected
 
 
 -- | Iso which inverts the current selection
 --
 -- @'inverting' = iso 'invertSelection' 'invertSelection'@
-inverting :: (Functor f, Functor g) => Iso (Selection f b a) (Selection g d c) (Selection f a b) (Selection g c d)
+inverting :: (Functor f) => Iso' (Selection f b a) (Selection f a b)
 inverting = dimap invertSelection (fmap invertSelection)
 
 -- | Iso which exposes the underlying functor representation
 --
 -- @'unwrapping' = iso 'unwrapSelection' 'wrapSelection'@
-unwrapping :: (Functor f) => Iso (Selection f b a) (Selection g d c) (f (Either b a)) (g (Either d c))
+unwrapping :: (Functor f) => Iso' (Selection f b a)  (f (Either b a))
 unwrapping = dimap unwrapSelection (fmap Selection)

a26778747040c6ad061dc8997d825491d8743cb0
Chris Penner <christopher.penner@gmail.com>
2019-06-11 11:24:51 +0200

    Updates

--- src/Control/Lens/Selection.hs
+++ src/Control/Lens/Selection.hs
@@ -9,21 +9,25 @@ module Control.Lens.Selection
 
 import Data.Profunctor (Profunctor(..))
 import Data.Functor.Selection
+import Data.Bitraversable
 
 type Traversal s t a b = forall f. Applicative f => (a -> f b) -> s -> f t
 type Traversal' s a = Traversal s s a a
 type Iso s t a b = forall p f. (Profunctor p, Functor f) => p a (f b) -> p s (f t)
 type Iso' s a = Iso s s a a
+type LensLike f s t a b = (a -> f b) -> s -> f t
 
 -- | Traversal over selected elements
 --
 -- @'selected' = 'traverse'@
+-- See also 'firsting'
 selected :: (Traversable f) => Traversal' (Selection f b a) a
 selected = traverse
 
 -- | Traversal over unselected elements
 --
 -- @'unselected' = 'inverting' . 'selected'@
+-- See also 'seconding'
 unselected :: (Traversable f) => Traversal' (Selection f b a) b
 unselected = inverting . selected
 
@@ -33,9 +37,14 @@ unselected = inverting . selected
 -- @'inverting' = iso 'invertSelection' 'invertSelection'@
 inverting :: (Functor f) => Iso' (Selection f b a) (Selection f a b)
 inverting = dimap invertSelection (fmap invertSelection)
+-- Note to self; write `Swapped` instance from 'lens'
 
 -- | Iso which exposes the underlying functor representation
 --
 -- @'unwrapping' = iso 'unwrapSelection' 'wrapSelection'@
 unwrapping :: (Functor f) => Iso' (Selection f b a)  (f (Either b a))
 unwrapping = dimap unwrapSelection (fmap Selection)
+
+-- | Given a functor as input
+selecting :: (Functor f) => (a -> Bool) -> Iso (f a) (f b) (Selection f a a) (Selection f b b)
+selecting predicate = dimap (select predicate . newSelection) (fmap forgetSelection)
